3567701c3eaba3311e4dbe089efb1843faed885a
hangeulBae <dxdiag20@gmail.com>
2019-07-08 18:31:38 +0900

    Remove Ipcvlab_Edu

--- Ipcvlab_Edu/Image Stitching/Merge.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include "opencv2/opencv.hpp"
-namespace Merge {
-	void MatrixMul(float homoCoord[1][3], float outputCoord[1][3], cv::Mat H);
-	void BackwardingMapping(cv::Mat &src, cv::Mat &dst, cv::Mat H);
-	void BackwardingMappingColor(cv::Mat &src1, cv::Mat &src2, cv::Mat &plate, cv::Mat H);
-}
